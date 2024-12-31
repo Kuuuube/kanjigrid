1e095077bf41c0dae1bec2f4eec5d5c7a5be1820
kuuuube <hexagonisalie@gmail.com>
2024-12-31 11:09:08 -0500

    Find data by script path in dedupe_data.py

--- tools/dedupe_data.py
+++ tools/dedupe_data.py
@@ -1,5 +1,6 @@
 import os
 import json
+import sys
 
 def dedupe_data(data_directory):
     for file in os.listdir(data_directory):
@@ -12,4 +13,4 @@ def dedupe_data(data_directory):
             data_json["data"][i][1] = "".join(kanji_list_deduped)
             json.dump(data_json, open(filepath, "w", encoding = "UTF8"), indent = 4, ensure_ascii = False)
 
-dedupe_data("../data")
+dedupe_data(sys.path[0] + "/../data")
