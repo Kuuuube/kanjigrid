2c7fe38f46cac125114cd6764c9d61ea178155dd
kuuuube <hexagonisalie@gmail.com>
2024-12-31 11:12:15 -0500

    Add start of data migration

--- data.py
+++ data.py
@@ -2,6 +2,8 @@ import os
 import collections
 import json
 
+GROUPING_JSON_VERSION = 1
+
 KanjiGroups = collections.namedtuple("KanjiGroups", ["name", "source", "lang", "data"])
 
 ignore = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz" + \
@@ -26,11 +28,14 @@ def load_from_folder(groups, path):
         filepath = path + "/" + file
         try:
             grouping_json = json.loads(open(filepath, "r", encoding = "utf-8").read())
+
+            if "version" not in grouping_json or GROUPING_JSON_VERSION > grouping_json["version"]:
+                grouping_json = migrate_data(grouping_json)
+
             groups.append(KanjiGroups(grouping_json["name"], grouping_json["source"], grouping_json["lang"], grouping_json["data"]))
         except Exception:
             # rethrow with msg in case a custom file in user_files is outdated
             raise Exception(f"Failed to load Kanji Grid data file \"{filepath}\". It might be corrupted or outdated.")
-
 
 def init_groups():
     global groups
@@ -45,3 +50,17 @@ def init_groups():
     load_from_folder(groups, user_data_folder)
 
     groups.sort(key = lambda group: group.lang + group.name)
+
+def migrate_data(grouping_json):
+    if "version" not in grouping_json:
+        grouping_json["version"] = 0
+
+    grouping_json_updates = [data_update_1]
+    if len(grouping_json_updates) > grouping_json["version"]:
+        for grouping_json_update in grouping_json_updates[grouping_json["version"]:]:
+            grouping_json = grouping_json_update(grouping_json)
+        grouping_json["version"] = GROUPING_JSON_VERSION
+    return grouping_json
+
+def data_update_1(grouping_json):
+    return grouping_json
---
+++ tools/migrate_json.py
@@ -0,0 +1,17 @@
+import sys
+# add main folder to path to allow accessing module there
+sys.path.append(sys.path[0] + "/../.")
+
+import os
+import json
+
+import data
+
+def migrate(data_directory):
+    for file in os.listdir(data_directory):
+        filepath = data_directory + "/" + file
+        data_json = json.load(open(filepath, "r", encoding = "UTF8"))
+        data_json = data.migrate_data(data_json)
+        json.dump(data_json, open(filepath, "w", encoding = "UTF8"), indent = 4, ensure_ascii = False)
+
+migrate(sys.path[0] + "/../data")
