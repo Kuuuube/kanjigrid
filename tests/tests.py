4da94696199ce3ac53ec6dc67a53bc318a5a5679
kuuuube <hexagonisalie@gmail.com>
2025-03-13 00:20:28 -0400

    Validate config in config test

--- tests/tests.py
+++ tests/tests.py
@@ -13,11 +13,12 @@ def test_config_update():
     config_current = json.load(open("./config.json", "r"))
 
     config_migrated = config_util.migrate_config(config_v0)
+    config_validated = config_util.validate_config(config_migrated)
 
-    config_migrated_string = json.dumps(config_migrated, sort_keys = True, indent = 4)
+    config_migrated_validated_string = json.dumps(config_validated, sort_keys = True, indent = 4)
     config_current_string = json.dumps(config_current, sort_keys = True, indent = 4)
 
-    assert(config_migrated_string == config_current_string)
+    assert(config_migrated_validated_string == config_current_string)
 
 def test_data_load():
     import data
