50d136aebc6dc10423c7ded6da46ef2c42ba9ff0
kuuuube <hexagonisalie@gmail.com>
2025-03-15 18:24:26 -0400

    Update dedupe_data tool for new data format

--- tools/dedupe_data.py
+++ tools/dedupe_data.py
@@ -2,15 +2,15 @@ import os
 import json
 import sys
 
-def dedupe_data(data_directory):
+def dedupe_groups_characters(data_directory):
     for file in os.listdir(data_directory):
         filepath = data_directory + "/" + file
         data_json = json.load(open(filepath, "r", encoding = "UTF8"))
-        for i, grouping_data in enumerate(data_json["data"]):
-            kanji_list = list(grouping_data[1])
+        for i, grouping_data in enumerate(data_json["groups"]):
+            kanji_list = list(grouping_data["characters"])
             kanji_list_deduped = list(dict.fromkeys(kanji_list))
 
-            data_json["data"][i][1] = "".join(kanji_list_deduped)
+            data_json["groups"][i]["characters"] = "".join(kanji_list_deduped)
             json.dump(data_json, open(filepath, "w", encoding = "UTF8"), indent = 4, ensure_ascii = False)
 
-dedupe_data(sys.path[0] + "/../data")
+dedupe_groups_characters(sys.path[0] + "/../data")
