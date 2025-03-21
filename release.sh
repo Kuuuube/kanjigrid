b0be116be9dcddbe8bb983a80e93c852e472a1d5
kuuuube <hexagonisalie@gmail.com>
2025-03-21 16:32:04 -0400

    Add manifest

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
 # Remove directories
-zip -j kanjigrid_kuuuube_0.0.0.zip src/*.py src/config.json LICENSE README.md
+zip -j kanjigrid_kuuuube_0.0.0.zip src/*.py src/config.json LICENSE README.md manifest.json
 # Keep directories
 zip kanjigrid_kuuuube_0.0.0.zip data/*
