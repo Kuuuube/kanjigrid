fd5f2ba1a37cdbb4db3c0a90d858e43339989e63
kuuuube <hexagonisalie@gmail.com>
2025-03-22 21:14:43 -0400

    Restructure release zip creation

--- release.sh
+++ release.sh
@@ -1,5 +1,8 @@
+# Reset
+rm -rf ./dist
 rm -f kanjigrid_kuuuube_0.0.0.zip
-# Remove directories
-zip -j kanjigrid_kuuuube_0.0.0.zip src/*.py src/config.json LICENSE README.md manifest.json
-# Keep directories
-zip kanjigrid_kuuuube_0.0.0.zip data/*
+mkdir ./dist
+
+# Create release zip
+cp -r src/*.py src/config.json data LICENSE README.md manifest.json ./dist
+(cd dist && zip ../kanjigrid_kuuuube_0.0.0.zip *)
